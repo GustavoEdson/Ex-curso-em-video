dd0df8a2810870a29fe8d5e1c43ec12583fa5c60
Nawaus <80435752+Nawaus@users.noreply.github.com>
2021-11-15 00:07:57 -0300

    Update ex019.py

--- ex019.py
+++ ex019.py
@@ -1,14 +1,14 @@
 #019
 '''programa que sorteia pessoas aleatorias UAU'''
 
-from random import choice
+import random
 
 n1 = input('Primeiro aluno: ')
 n2 = input('Segundo aluno: ')
 n3 = input('Terceiro aluno: ')
 n4 = input('Quarto aluno: ')
 
-lista = [n1, n2, n3, n4]
+ganhador = [n1, n2, n3, n4]
 
-choice(lista)
-print('O ganhador foi {}'.format(lista))
+escolhido = random.choice(ganhador)
+print('O ganhador foi {}'.format(escolhido))
